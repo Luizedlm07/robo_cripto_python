6aba4c73d8e695798cd0084df4293a45dfaebfda
Luizedlm07 <luiz.edlm07@gmail.com>
2023-01-14 20:05:49 -0300

    Tentativa de criar um dict em 'dados_relatorio'

--- dados_relatorio.py
+++ dados_relatorio.py
@@ -6,10 +6,12 @@ def dados_relatorio():
         dado = csv.reader(dados, delimiter = ",")
 
         for linha in dado:
-            ultimo_trade_moeda = linha[0][12:18]
+            ultimo_trade_moeda = dict(linha)
+            print(ultimo_trade_moeda)
             ultimo_trade = linha[12][9:15]
             ultimo_preco = linha[14][22:32]
             ultima_qty_par_1 = float(linha[7][17:-1])
             ultima_qty_par_2 = float(linha[8][25:-1])
+
 
     return ultimo_trade_moeda, ultimo_trade, ultimo_preco, ultima_qty_par_1, ultima_qty_par_2
